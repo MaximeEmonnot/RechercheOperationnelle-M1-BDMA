92bde35cd71630a8e0dfa5907397b2e9c52ef432
Alexandre Chanson <a.chanson.tours@gmail.com>
2022-10-21 15:49:34 +0200

    more instances

--- src/main/java/com/alexscode/teaching/Main.java
+++ src/main/java/com/alexscode/teaching/Main.java
@@ -11,6 +11,11 @@ public class Main {
     public static void main(String[] args) {
         Instance f4_small = Instance.readFile("./instances/f4_tap_0_20.dat", 330, 27);
 
+        Instance f4_1_big = Instance.readFile("./instances/f4_tap_1_400.dat", 6600, 540);
+        Instance f4_4_big = Instance.readFile("./instances/f4_tap_4_400.dat", 6600, 540);
+        Instance f1_3_big = Instance.readFile("./instances/f1_tap_3_400.dat", 6600, 540);
+        Instance f1_9_big = Instance.readFile("./instances/f1_tap_9_400.dat", 6600, 540);
+
         Objectives obj = new Objectives(f4_small);
 
         TAPSolver solver = new TestNaif();
